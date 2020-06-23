1a28ffab91a7894b37df104d8865444d0d42bff9
d-saori <60836449+d-saori@users.noreply.github.com>
2020-06-23 18:31:58 +0900

    add: いいね機能修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  # message.liked_users で message を「いいね!」しているユーザー一覧が取得できるようになる
+  # through: :likes→中間テーブルの部分
+  has_many :liked_users, through: :likes, source: :user
   # presence: true→空の文字列は入力できなくなる
   validates :content, presence: true, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # dependent: :destroy→この表記がないとアカウントを削除しても投稿メッセージ(アソシエーションされたuser.messages)が残ってしまう
   has_many :messages, dependent: :destroy
   has_many :likes, dependent: :destroy
+  # user.liked_messages で user が「いいね!」しているメッセージ一覧が取得できるようになる
+  has_many :liked_messages, through: :likes, source: :message
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
